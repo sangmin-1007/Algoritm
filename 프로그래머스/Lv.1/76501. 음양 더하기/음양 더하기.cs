3b2ba314f768cbda9ae69c0c35c3380bea2faf4c
sangmin-1007 <154484912+sangmin-1007@users.noreply.github.com>
2024-01-12 09:52:21 +0900

    [level Lv.1] Title: 음양 더하기, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/76501. 음양 더하기/음양 더하기.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public int solution(int[] absolutes, bool[] signs) {
+        int answer = 0;
+        for(int i = 0; i < absolutes.Length; i++)
+        {
+            if(!signs[i])
+            {
+                absolutes[i] = -absolutes[i];
+            }
+        }
+        
+        for(int i = 0; i < absolutes.Length; i++)
+        {
+            answer += absolutes[i];
+        }
+        return answer;
+    }
+}
