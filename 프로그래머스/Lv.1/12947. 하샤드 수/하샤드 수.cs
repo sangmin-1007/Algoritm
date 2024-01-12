38a687cff0143fac8c1a8f56f5df3346f2815771
sangmin-1007 <154484912+sangmin-1007@users.noreply.github.com>
2024-01-12 09:38:18 +0900

    [level Lv.1] Title: 하샤드 수, Time: 0.67 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12947. 하샤드 수/하샤드 수.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public bool solution(int x) {
+        bool answer = true;
+        string temp = x.ToString();
+        int sum = 0;
+        for(int i = 0; i<temp.Length; i++)
+        {
+            sum += (int)Char.GetNumericValue(temp,i);
+        }
+        
+        if(x%sum == 0)
+            return answer;
+        else
+        {
+            answer = false;
+            return answer;
+        }
+        
+    }
+}
