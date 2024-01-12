8051dc2aaea817efeb495a8c37b2fb1046f5fb12
sangmin-1007 <154484912+sangmin-1007@users.noreply.github.com>
2024-01-12 09:11:11 +0900

    [level Lv.0] Title: 나이 출력, Time: 0.13 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120820. 나이 출력/나이 출력.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        int year = 2022;
+        answer = year - age + 1;
+        return answer;
+    }
+}
