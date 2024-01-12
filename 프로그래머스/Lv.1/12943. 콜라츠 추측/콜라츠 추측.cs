1ad4fe1e9d185bc7387ff2f34287ed3072d5f9aa
sangmin-1007 <154484912+sangmin-1007@users.noreply.github.com>
2024-01-12 09:41:30 +0900

    [level Lv.1] Title: 콜라츠 추측, Time: 0.26 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long temp = num;
+        for(int i = 0; i <= 500; i++)
+        {
+            if(temp == 1)
+            {
+                return i;
+            }
+            
+            temp = temp % 2 == 0 ? temp / 2 : temp * 3 + 1;
+        }
+        answer = -1;
+        return answer;
+    }
+}
