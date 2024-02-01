c2bc6477169c4d34160a78d65547d79d4f4f46a9
sangmin-1007 <154484912+sangmin-1007@users.noreply.github.com>
2024-02-01 09:53:16 +0900

    [level 1] Title: 3진법 뒤집기, Time: 3.01 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cs
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cs
@@ -1,14 +1,19 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        while(n > 0)
+        List<int> list = new List<int>();
+        while (n != 0)
         {
-            answer *= 3;
-            answer += n%3;
-            n/=3;
+            list.Add(n % 3);
+            n /= 3;
         }
+
+        list.Reverse();
+
+        return (int)list.Select((x, idx) => x * Math.Pow(3, idx)).Sum();
         return answer;
     }
 }
