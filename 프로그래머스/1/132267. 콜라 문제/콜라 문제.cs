d38abc98787f32f9fcbe55550f1dd705f7c6cd38
sangmin-1007 <154484912+sangmin-1007@users.noreply.github.com>
2024-03-14 20:52:51 +0900

    [level 1] Title: 콜라 문제, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -4,12 +4,11 @@ public class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
         
-        while (n > a - 1)
+        while(n >= a)
         {
-            answer += (n / a) * b;
+            answer += n / a * b;
             n = ((n / a) * b) + (n % a);
         }
-        
         return answer;
     }
 }
