3e4675474f1c9b70855f41f9b13bdb8d9dc932d6
sangmin-1007 <154484912+sangmin-1007@users.noreply.github.com>
2024-03-19 21:18:46 +0900

    [level 1] Title: 2016년, Time: 0.25 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public string solution(int a, int b) {
+        string answer = "";
+        
+        string[] day = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int[] days = {31,29,31,30,31,30,31,31,30,31,30,31};
+        
+        int totalDays = 0;
+        
+        for(int i = 0; i < a-1; i++)
+        {
+            totalDays += days[i];
+        }
+        
+        totalDays += b - 1;
+        answer = day[totalDays % 7];
+        return answer;
+    }
+}
