d71071ea74cb5e7fd6d15dd059d88f7ae87520b7
sangmin-1007 <154484912+sangmin-1007@users.noreply.github.com>
2024-04-04 22:12:40 +0900

    [level 1] Title: 숫자 짝꿍, Time: 2.35 ms, Memory: 30.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+    
+public class Solution {
+    public string solution(string X, string Y) {
+        List<char> answer = new List<char>();
+        List<char> xList = X.ToList();
+        List<char> yList = Y.ToList();
+        
+        xList.Sort();
+        yList.Sort();
+        
+        int indexX = 0;
+        int indexY = 0;
+        
+        while(indexX < xList.Count && indexY < yList.Count)
+        {
+            if(xList[indexX] == yList[indexY])
+            {
+                answer.Add(xList[indexX]);
+                ++indexX;
+                ++indexY;
+            }
+            else if(xList[indexX] != yList[indexY])
+            {
+                if(xList[indexX] < yList[indexY])
+                {
+                    ++indexX;
+                }
+                else
+                {
+                    ++indexY;
+                }
+            }
+        }
+        
+        if(answer.Count == 0)
+        {
+            return "-1";
+        }
+        else if(answer[answer.Count() - 1] == '0')
+        {
+            return "0";
+        }
+        
+        answer.Reverse();
+        string str = new string(answer.ToArray());
+        
+        return str;
+    }
+}
