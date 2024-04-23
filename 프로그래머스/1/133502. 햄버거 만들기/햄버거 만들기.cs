c563aba94a9de12c360185ee1235c6a9d23707bf
sangmin-1007 <154484912+sangmin-1007@users.noreply.github.com>
2024-04-23 11:32:58 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.73 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        List<int> ingredList = new List<int>();
+        
+        foreach(int item in ingredient)
+        {
+            ingredList.Add(item);
+            if(ingredList.Count >= 4)
+            {
+                if(ingredList[ingredList.Count-4] == 1 && ingredList[ingredList.Count-3] == 2 &&
+                  ingredList[ingredList.Count-2] == 3 && ingredList[ingredList.Count-1] == 1)
+                {
+                    answer++;
+                    ingredList.RemoveRange(ingredList.Count-4,4);
+                }
+            }
+        }
+        return answer;
+    }
+}
