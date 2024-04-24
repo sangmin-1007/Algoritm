3ef60c1800f0e4bd165d660f2f1d93e1aa46e1e4
sangmin-1007 <154484912+sangmin-1007@users.noreply.github.com>
2024-04-24 20:53:17 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 3.88 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+public class Solution {
+    public string solution(string[] survey, int[] choices) {
+        string answer = "";
+        int count = 0;
+        Dictionary<char, int> score = new Dictionary<char, int>()
+        {
+            {'R',0 },{'T',0 },{'C',0 },{'F',0 },{'J',0 },{'M',0 },{'A',0 },{'N',0 },
+        };
+        Dictionary<int, int> addScore = new Dictionary<int, int>()
+        {
+            { 1,3 },{ 2,2 },{ 3,1 },{ 4,0 },{ 5,-1 },{ 6,-2 },{ 7,-3 }
+        };
+        
+        for(int i = 0; i < survey.Length; i++)
+        {
+            score[survey[i][0]] += addScore[choices[i]];
+        }
+        
+        if (score['R'] >= score['T'])
+        {
+            answer += "R";
+        }
+        else
+        {
+            answer += "T";
+        } 
+
+        if (score['C'] >= score['F'])
+        {
+            answer += "C";
+        }
+        else
+        {
+            answer += "F";
+        }
+
+        if (score['J'] >= score['M'])
+        {
+            answer += "J";
+        }
+        else
+        {
+            answer += "M";
+        }
+
+        if (score['A'] >= score['N'])
+        {
+            answer += "A";
+        }
+        else
+        {
+            answer += "N";
+        }
+        
+        return answer;
+    }
+}
